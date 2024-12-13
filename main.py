3324e327f2b7def6331d92739e6c36e72a9359fe
nmlakra <nikhilmarklakra@gmail.com>
2024-12-14 03:15:46 +0530

    refactored code into groups

--- main.py
+++ main.py
@@ -4,29 +4,41 @@ from constants import *
 
 
 def main():
-    # Initializing pygame 
+    # Initializing pygame
     pygame.init()
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     x_player_spwan_location = SCREEN_WIDTH / 2
     y_player_spwan_location = SCREEN_HEIGHT / 2
     player = Player(x_player_spwan_location, y_player_spwan_location)
 
-    clock = pygame.time.Clock() 
+    clock = pygame.time.Clock()
     dt = 0
     # GUI Window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Event loop
     while True:
+
         for event in pygame.event.get():
-            player.update(dt)
             if event.type == pygame.QUIT:
                 return
+
+        for updatable_object in updatable:
+            updatable_object.update(dt)
+
         screen.fill(BLACK)
-        player.draw(screen)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+
         pygame.display.flip()
 
+        # Limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
-    
+
+
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -4,12 +4,12 @@ import pygame
 
 
 class Player(CircleShape):
-    
+
     def __init__(self, x, y, radius=PLAYER_RADIUS):
         super().__init__(x, y, radius)
 
         self.rotation = 0
-    
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -17,7 +17,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    
+
     def draw(self, screen):
         pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
 
@@ -28,15 +28,15 @@ class Player(CircleShape):
         self.rotate(dt)
 
     def rotate_right(self, dt):
-        self.rotate(dt * -1) 
+        self.rotate(dt * -1)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-    
+
     def move_forward(self, dt):
         self.move(dt)
-        
+
     def move_backward(self, dt):
         self.move(dt * -1)
 
